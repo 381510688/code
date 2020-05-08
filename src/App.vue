415acb06f6410064c4b908b6da656e5955136437
李刚 <381510688@users.noreply.github.com>
2020-05-08 17:17:26 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <hello-world :person="{name: 'ligang'}"></hello-world>
+    <input v-model="name">
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      name: ""
+    };
+  }
+};
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div>Hello {{person.name}}</div>
+    <div>Updated次数：{{count}}</div>
+  </div>
+</template>
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    person: Object
+  },
+  data() {
+    return {
+      count: 0
+    };
+  },
+  updated() {
+    this.count++;
+  }
+};
+</script>
